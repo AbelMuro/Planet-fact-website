ce11c251b8853bfcad3dc1e600fc88ac468aa3f7
abelmuro <abelmuro93@gmail.com>
2024-02-18 13:29:18 -0800

    commit

--- src/Components/Planet/MobileButtons/MobileButtons.js
+++ src/Components/Planet/MobileButtons/MobileButtons.js
@@ -2,50 +2,57 @@ import React, {useEffect} from 'react';
 import styles from './styles.module.css';
 import {useSelector} from 'react-redux';
 
-function MobileButtons({setSelectedButton}) {
+function MobileButtons({selectedButton, setSelectedButton}) {
     const theme = useSelector(state => state.theme);
 
     const handleClick = (e) => {
         if(!e.target.matches('button')) return;
-
         let id = e.target.getAttribute('data-id');
         setSelectedButton(id)
     }
 
-    const handleEnter = (e) => {
-        let line = e.target.nextElementSibling;
-        line.style.opacity = '1'
-    }
-
-    const handleLeave = (e) => {
-        let line = e.target.nextElementSibling;
-        line.style.opacity = '';
-    }
-
     useEffect(() => {
         let lines = document.querySelectorAll('.' + styles.line);
-
         lines.forEach((line) => {
-            line.style.backgroundColor = theme
+            line.style.backgroundColor = theme;
         })
     }, [theme])
+
+
+    useEffect(() => {
+        let buttons = document.querySelectorAll('.' + styles.button);
+
+
+        buttons.forEach((button) => {
+            button.style.color = '';
+            button.nextElementSibling.style.opacity = '';
+        })
+
+        buttons.forEach((button) => {
+            let id = button.getAttribute('data-id');
+            if(selectedButton === id){
+                button.style.color = 'white';
+                button.nextElementSibling.style.opacity = '1';
+            }
+        })
+    }, [selectedButton])
 
     return(
         <ul className={styles.buttons} onClick={handleClick}>
             <li>
-                <button className={styles.button} onMouseEnter={handleEnter} onMouseLeave={handleLeave} data-id='overview'>
+                <button className={styles.button} data-id='overview'>
                     overview
                 </button>    
                 <div className={styles.line}></div>            
             </li>
             <li>
-                <button className={styles.button} onMouseEnter={handleEnter} onMouseLeave={handleLeave} data-id='structure'>
+                <button className={styles.button} data-id='structure'>
                     structure
                 </button>       
                 <div className={styles.line}></div>            
             </li>
             <li>
-                <button className={styles.button} onMouseEnter={handleEnter} onMouseLeave={handleLeave} data-id='surface'>
+                <button className={styles.button} data-id='surface'>
                     surface
                 </button>       
                 <div className={styles.line}></div>                 
--- src/Components/Planet/Planet.js
+++ src/Components/Planet/Planet.js
@@ -89,7 +89,7 @@ function Planet() {
         }   
         else if(tablet){
             planetImageRef.current.style.width = planetData.imageSizes.tablet;    
-            planetImageRef.current.style.height = '100%';
+            planetImageRef.current.style.height = '460px';
         }
         else {
             planetImageRef.current.style.height = planetData.imageSizes.desktop;
@@ -100,81 +100,84 @@ function Planet() {
 
 
     return(
-        <article className={styles.planet}>
-            {mobile && <MobileButtons setSelectedButton={setSelectedButton}/>}
-            <section className={styles.planet_content}>
-                <div className={styles.planet_image} ref={planetImageRef}>
-                    {selectedButton === 'surface' && <img className={styles.planet_geology} src={icons[selectedPlanet]}/>}
-                </div>
-                <div className={styles.planet_details}>
-                    <h1 className={styles.planet_title}>
-                        {planetData.name && planetData.name}
-                    </h1>
-                    <p className={styles.planet_desc}>
-                        {setPlanetDesc()}
-                    </p>
-                    <div className={styles.planet_link}>
-                        Source : 
-                        <a target='_blank' href={setSourceLink()}> Wikipedia</a>
-                        <img className={styles.square_icon} src={icons['linkSquare']}/>
+        <>
+            {mobile && <MobileButtons selectedButton={selectedButton} setSelectedButton={setSelectedButton}/>}
+            <article className={styles.planet}>
+                <section className={styles.planet_content}>
+                    <div className={styles.planet_image} ref={planetImageRef}>
+                        {selectedButton === 'surface' && <img className={styles.planet_geology} src={icons[selectedPlanet]}/>}
                     </div>
-                    {!mobile && <div className={styles.planet_buttons} onClick={handleImage} ref={allButtonsRef}>
-                        <button className={styles.planet_button} data-id='overview'>
-                            <span>
-                                01
-                            </span>
-                            overview
-                        </button>
-                        <button className={styles.planet_button} data-id='structure'>
-                            <span>
-                                02
-                            </span>
-                            internal structure
-                        </button>
-                        <button className={styles.planet_button} data-id='surface'>
-                            <span>
-                                03
-                            </span>
-                            surface geology
-                        </button>
-                    </div> }                   
-                </div>
-            </section>
-            <section className={styles.planet_additionalInfo}>
-                <div className={styles.planet_info}>
-                    <h2>
-                        rotation time
-                    </h2>
-                    <strong>
-                        {planetData.rotation && planetData.rotation}
-                    </strong>
-                </div>
-                <div className={styles.planet_info}>
-                    <h2>
-                        revolution time
-                    </h2>
-                    <strong>
-                        {planetData.revolution && planetData.revolution}
-                    </strong>
-                </div>
-                <div className={styles.planet_info}>
-                    <h2>
-                        radius
-                    </h2>
-                    <strong>
-                        {planetData.radius && planetData.radius}
-                    </strong>
-                </div>
-                <div className={styles.planet_info}>
-                    <h2>
-                        average temp
-                    </h2>
-                    <strong>
-                        {planetData.temperature && planetData.temperature}
-                    </strong>
-                </div>
-            </section>
-        </article>
+                    <div className={styles.planet_details}>
+                        <h1 className={styles.planet_title}>
+                            {planetData.name && planetData.name}
+                        </h1>
+                        <p className={styles.planet_desc}>
+                            {setPlanetDesc()}
+                        </p>
+                        <div className={styles.planet_link}>
+                            Source : 
+                            <a target='_blank' href={setSourceLink()}> Wikipedia</a>
+                            <img className={styles.square_icon} src={icons['linkSquare']}/>
+                        </div>
+                        {!mobile && <div className={styles.planet_buttons} onClick={handleImage} ref={allButtonsRef}>
+                            <button className={styles.planet_button} data-id='overview'>
+                                <span>
+                                    01
+                                </span>
+                                overview
+                            </button>
+                            <button className={styles.planet_button} data-id='structure'>
+                                <span>
+                                    02
+                                </span>
+                                internal structure
+                            </button>
+                            <button className={styles.planet_button} data-id='surface'>
+                                <span>
+                                    03
+                                </span>
+                                surface geology
+                            </button>
+                        </div> }                   
+                    </div>
+                </section>
+                <section className={styles.planet_additionalInfo}>
+                    <div className={styles.planet_info}>
+                        <h2>
+                            rotation time
+                        </h2>
+                        <strong>
+                            {planetData.rotation && planetData.rotation}
+                        </strong>
+                    </div>
+                    <div className={styles.planet_info}>
+                        <h2>
+                            revolution time
+                        </h2>
+                        <strong>
+                            {planetData.revolution && planetData.revolution}
+                        </strong>
+                    </div>
+                    <div className={styles.planet_info}>
+                        <h2>
+                            radius
+                        </h2>
+                        <strong>
+                            {planetData.radius && planetData.radius}
+                        </strong>
+                    </div>
+                    <div className={styles.planet_info}>
+                        <h2>
+                            average temp
+                        </h2>
+                        <strong>
+                            {planetData.temperature && planetData.temperature}
+                        </strong>
+                    </div>
+                </section>
+            </article>        
+        </>
+
     )
 }
 
